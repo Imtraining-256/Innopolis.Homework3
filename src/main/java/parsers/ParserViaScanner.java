19675f88b8b4baf3770550297304d114550d615b
atenyaeves <atenyaev@protonmail.com>
2021-09-30 22:04:40 +0700

    Fix regex for scanner

--- src/main/java/parsers/ParserViaScanner.java
+++ src/main/java/parsers/ParserViaScanner.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class ParserViaScanner implements Parser {
 
-    private static final String WORD_TO_FIND = "[Сс]трада";
+    private static final String WORD_TO_FIND = "[Сс]трада\\S*\\b";
     private final Pattern pattern = Pattern.compile(WORD_TO_FIND);
 
     @Override
